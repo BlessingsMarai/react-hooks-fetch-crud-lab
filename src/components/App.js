e5a7e3ae17c4c2c66bc10063b451932a998a057a
Blessings <blessings.marai@student.moringaschool.com>
2023-11-03 06:57:07 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,77 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
+
+  const fetchQuestions = () => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data))
+      ;
+  };
+
+  const addQuestion = (formData) => {
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setQuestions([...questions, data]);
+        setPage("List");
+      })
+      ;
+  };
+
+  const deleteQuestion = (id) => {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => setQuestions(questions.filter((question) => question.id !== id)))
+      ;
+  };
+
+  const updateCorrectAnswer = (id, correctIndex) => {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex }),
+    })
+      .then(() => {
+        setQuestions((prevQuestions) =>
+          prevQuestions.map((question) =>
+            question.id === id ? { ...question, correctIndex } : question
+          )
+        );
+      })
+      ;
+  };
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm addQuestion={addQuestion} />
+      ) : (
+        <QuestionList
+          questions={questions}
+          deleteQuestion={deleteQuestion}
+          updateCorrectAnswer={updateCorrectAnswer}
+        />
+      )}
     </main>
   );
 }
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, deleteQuestion, updateCorrectAnswer }) {
   const { id, prompt, answers, correctIndex } = question;
+
+  const [selectedCorrectAnswer, setSelectedCorrectAnswer] = useState(correctIndex);
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -9,15 +11,29 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  const handleDelete = () => {
+    // Call the deleteQuestion function and pass the question ID
+    deleteQuestion(id);
+  };
+
+  const handleCorrectAnswerChange = (event) => {
+    const newCorrectIndex = parseInt(event.target.value, 10);
+    setSelectedCorrectAnswer(newCorrectIndex);
+    // Call the updateCorrectAnswer function to update the correct answer
+    updateCorrectAnswer(id, newCorrectIndex);
+  };
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select value={selectedCorrectAnswer} onChange={handleCorrectAnswerChange}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
